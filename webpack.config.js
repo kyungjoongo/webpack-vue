b18953ac113b05d87ae9df4ffb088af777190aaf
Basil Safwat <bsafwat@gmail.com>
2017-11-07 11:09:06 +0000

    Add views alias (and reformat)

--- webpack.config.js
+++ webpack.config.js
@@ -1,183 +1,189 @@
-let path = require('path'),
-    webpack = require('webpack'),
-    CopyWebpackPlugin = require('copy-webpack-plugin'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    FaviconsWebpackPlugin = require('favicons-webpack-plugin'),
-    CleanWebpackPlugin = require('clean-webpack-plugin'),
-    FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+let path = require("path"),
+  webpack = require("webpack"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  FaviconsWebpackPlugin = require("favicons-webpack-plugin"),
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin")
 
-let ICON = path.join(__dirname, './source/img/icon.png');
-let NODE_ENV = process.env.NODE_ENV;
+let ICON = path.join(__dirname, "./source/img/icon.png")
+let NODE_ENV = process.env.NODE_ENV
 
-var imageLoader = ['file-loader?name=img/[name]-[hash:7].[ext]'];
+var imageLoader = ["file-loader?name=img/[name]-[hash:7].[ext]"]
 // Optimize images for production build
 
-if (NODE_ENV === 'production') {
-    imageLoader.push({
-        loader: 'image-webpack-loader',
-        query: {
-            mozjpeg: {
-                progressive: true,
-            },
-            gifsicle: {
-                interlaced: false,
-            },
-            optipng: {
-                optimizationLevel: 4,
-            },
-            pngquant: {
-                quality: '75-90',
-                speed: 3,
-            },
-        },
-    });
+if (NODE_ENV === "production") {
+  imageLoader.push({
+    loader: "image-webpack-loader",
+    query: {
+      mozjpeg: {
+        progressive: true
+      },
+      gifsicle: {
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 4
+      },
+      pngquant: {
+        quality: "75-90",
+        speed: 3
+      }
+    }
+  })
 }
 
-
 module.exports = {
-    entry: {
-        js: './source/js/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'www/'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            options: {
-                loaders: {
-                    'scss': 'vue-style-loader!css-loader!sass-loader',
-                    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-                },
-                transformToRequire: {
-                    video: ['src','poster'],
-                    source: 'src'
-                }
-            }
-        }, {
-            test: /\.js$/,
-            loader: 'babel-loader?cacheDirectory',
-            exclude: /node_modules/,
-            options: {
-                presets: ["es2015", "stage-2"],
-                plugins: ['transform-runtime']
-            }
-
-        }, {
-            test: /\.css$/,
-            loaders: ['style-loader', 'css-loader']
-        }, {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader?sourceMap']
-        }, {
-            test: /\.(otf|ttf|woff)$/,
-            loader: 'file-loader?name=fonts/[name].[ext]'
-        }, {
-            test: /\.(mpeg|mp4|webm|ogv)$/,
-            loader: 'file-loader?name=videos/[name].[ext]'
-        }, {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            loaders: imageLoader
-        }, {
-            test: /\.(wav|mp3)$/,
-            loader: 'file-loader?name=sounds/[name].[ext]&context=./source/sounds'
-        }]
-    },
-    resolve: {
-        extensions: ['.js', '.json', '.vue', '.scss'],
-        alias: {
-            'vue$': 'vue/dist/vue.common.js',
-            'components': path.resolve(__dirname, 'source/components'),
-            'sass': path.resolve(__dirname, 'source/sass'),
-            'js': path.resolve(__dirname, 'source/js')
+  entry: {
+    js: "./source/js/main.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "www/"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {
+            scss: "vue-style-loader!css-loader!sass-loader",
+            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
+          },
+          transformToRequire: {
+            video: ["src", "poster"],
+            source: "src"
+          }
         }
-    },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true,
-        host: '0.0.0.0',
-        port: 3000,
-        quiet: true,
-        overlay: true
-    },
-    performance: {
-        hints: false
-    },
-    devtool: 'eval',
-    plugins: [
-        new FriendlyErrorsWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'source/index.html',
-            hash: true
-        }),
-        new FaviconsWebpackPlugin({
-            logo: './source/img/icon.png',
-            prefix: 'favicon/[hash]-',
-            emitStats: false,
-            statsFilename: 'iconstats-[hash].json',
-            persistentCache: true,
-            inject: true,
-            title: 'Prototype',
-            icons: {
-                android: false,
-                appleIcon: true,
-                appleStartup: false,
-                coast: false,
-                favicons: true,
-                firefox: false,
-                opengraph: false,
-                twitter: false,
-                yandex: false,
-                windows: false
-            }
-        })
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader?cacheDirectory",
+        exclude: /node_modules/,
+        options: {
+          presets: ["es2015", "stage-2"],
+          plugins: ["transform-runtime"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ["style-loader", "css-loader", "sass-loader?sourceMap"]
+      },
+      {
+        test: /\.(otf|ttf|woff)$/,
+        loader: "file-loader?name=fonts/[name].[ext]"
+      },
+      {
+        test: /\.(mpeg|mp4|webm|ogv)$/,
+        loader: "file-loader?name=videos/[name].[ext]"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: imageLoader
+      },
+      {
+        test: /\.(wav|mp3)$/,
+        loader: "file-loader?name=sounds/[name].[ext]&context=./source/sounds"
+      }
     ]
+  },
+  resolve: {
+    extensions: [".js", ".json", ".vue", ".scss"],
+    alias: {
+      vue$: "vue/dist/vue.common.js",
+      components: path.resolve(__dirname, "source/components"),
+      views: path.resolve(__dirname, "source/views"),
+      sass: path.resolve(__dirname, "source/sass"),
+      js: path.resolve(__dirname, "source/js")
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    host: "0.0.0.0",
+    port: 3000,
+    quiet: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: "eval",
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "source/index.html",
+      hash: true
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./source/img/icon.png",
+      prefix: "favicon/[hash]-",
+      emitStats: false,
+      statsFilename: "iconstats-[hash].json",
+      persistentCache: true,
+      inject: true,
+      title: "Prototype",
+      icons: {
+        android: false,
+        appleIcon: true,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    })
+  ]
 }
 
 // Fix for error messages
-process.noDeprecation = true;
+process.noDeprecation = true
 
-
-if (NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map'
-        // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new CleanWebpackPlugin(['www'], {
-            exclude: ['favicon']
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            comments: false,
-            compress: {
-                warnings: false
-            }
-        }),
-        // Required for ios add-t-home icon
-        new CopyWebpackPlugin([
-            { from: './source/server/.htaccess', to: 'favicon/' }
-        ])
+if (NODE_ENV === "production") {
+  module.exports.devtool = "#source-map"
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new CleanWebpackPlugin(["www"], {
+      exclude: ["favicon"]
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      comments: false,
+      compress: {
+        warnings: false
+      }
+    }),
+    // Required for ios add-t-home icon
+    new CopyWebpackPlugin([
+      { from: "./source/server/.htaccess", to: "favicon/" }
     ])
+  ])
 }
 
-
-if (NODE_ENV === 'fast') {
-    module.exports.devtool = '#eval'
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new CopyWebpackPlugin([
-            { from: './source/server/.htaccess', to: 'favicon/' }
-        ])
+if (NODE_ENV === "fast") {
+  module.exports.devtool = "#eval"
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: "./source/server/.htaccess", to: "favicon/" }
     ])
+  ])
 }
